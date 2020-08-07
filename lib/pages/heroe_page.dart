e8e35d55af019d1a446c6e7b63ebeb146687b08f
mari <mari.17ymy@gmail.com>
2020-08-06 23:40:15 -0400

    tarjeta con imgen de heroe y villano

--- lib/pages/heroe_page.dart
+++ lib/pages/heroe_page.dart
@@ -7,19 +7,49 @@ class HeroePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final heroeProvider = Provider.of<HeroeProvider>(context);
+    
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('id: ${heroeProvider.id}'),
-            Text('nombre: ${heroeProvider.nombre}'),
-            Text('alias: ${heroeProvider.alias}'),
-            Text('poderes: ${heroeProvider.poderes}'),
-            Text('imagen Url: ${heroeProvider.imagenUrl}')
+            _Tarjeta()
           ],
         )
+      ),
+    );
+  }
+}
+
+class _Tarjeta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final heroeProvider = Provider.of<HeroeProvider>(context);
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              child: Image(
+                image: NetworkImage(heroeProvider.imagenUrl),
+                fit: BoxFit.cover,
+              ),
+              width: double.infinity,
+              height: 210.0,
+              color: Colors.black54,
+            ),
+            Container(
+             alignment: Alignment.center,
+             child: Text(heroeProvider.nombre, style: TextStyle(fontSize: 30.0, color: Colors.white70)),
+             width: double.infinity,
+             height: 50.0,
+             color: Colors.white24,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/villano_page.dart
+++ lib/pages/villano_page.dart
@@ -6,20 +6,59 @@ class VillanoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final villanoProvider = Provider.of<VillanoProvider>(context);
+    
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('id: ${villanoProvider.id}'),
-            Text('nombre: ${villanoProvider.nombre}'),
-            Text('alias: ${villanoProvider.alias}'),
-            Text('poderes: ${villanoProvider.poderes}'),
-            Text('imagen Url: ${villanoProvider.imagenUrl}')
-          ],
-        )
+      body: Column(
+        children: [
+          _Tarjeta()
+        ],
       ),
     );
   }
 }
+
+class _Tarjeta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final villanoProvider = Provider.of<VillanoProvider>(context);
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              child: Image(
+                image: NetworkImage(villanoProvider.imagenUrl),
+                fit: BoxFit.cover,
+              ),
+              width: double.infinity,
+              height: 210.0,
+              color: Colors.black54,
+            ),
+            Container(
+             alignment: Alignment.center,
+             child: Text(villanoProvider.nombre, style: TextStyle(fontSize: 30.0, color: Colors.white70)),
+             width: double.infinity,
+             height: 50.0,
+             color: Colors.white24,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+      //Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Text('id: ${villanoProvider.id}'),
+        //     Text('nombre: ${villanoProvider.nombre}'),
+        //     Text('alias: ${villanoProvider.alias}'),
+        //     Text('poderes: ${villanoProvider.poderes}'),
+        //     Text('imagen Url: ${villanoProvider.imagenUrl}')
+        //   ],
+        // )
